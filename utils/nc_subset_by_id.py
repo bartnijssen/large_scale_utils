88f8f9c7c9fb5cf0f4639746545d3093f7294ca9
Bart Nijssen <nijssen@uw.edu>
2019-08-23 14:57:10 -0600

    Do not change variables that do not vary along the subset dimension

--- utils/nc_subset_by_id.py
+++ utils/nc_subset_by_id.py
@@ -29,12 +29,28 @@ if __name__ == '__main__':
     # read the IDs to subset
     with open(args.idfile) as f:
         ids = [int(x) for x in f]
-    
+
+    # subset dimension
+    subset_dim = 'hru'
+
     # ingest the netcdf file
     ds = xr.open_dataset(args.ncfile)
 
+    # determine which variables do not vary along any of the subsetted dimensions
+    vars_without = []
+    for var in ds.variables:
+        if not subset_dim in ds[var].dims:
+            vars_without.append(var)
+
+    # take the other variables
+    vars_with = list(set(ds.variables).difference(set(vars_without)))
+
     # subset the netcdf file based on the hruId
-    ds_subset = ds.where(ds.hruId.isin(ids), drop=True)
+    ds_with = ds[vars_with]
+    ds_subset = ds_with.where(ds_with.hruId.isin(ids), drop=True)
+
+    # merge the with and without
+    ds_subset = ds_subset.merge(ds[vars_without])
 
     # make sure that the subsetted types are the same as the original ones
     for var in ds_subset.variables:
